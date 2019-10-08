571315f1b3ee340ff9cd73fbda7dde95cf994d24
Dan Gold <tentativechaos@gmail.com>
2019-10-07 20:17:58 -0700

    Added support for using a .env file in order for previous change to actually work.

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
+// Setup access to custom environmntal variables
+require("dotenv").config();
+
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
